44d173c29669221c04f105c1341b9414f7f2c296
Jack Franklin <jack@jackfranklin.net>
2015-11-28 17:39:40 +0000

    Tidying up

--- describe.js
+++
@@ -1,7 +0,0 @@
-let assertions = [];
-
-function describe(name, fn) {
-  assertions.push({ name, fn });
-}
-
-export { describe, assertions };
--- runner.js
+++ runner.js
@@ -1,44 +1,52 @@
 import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs';
-import { assertions } from './describe';
 
-fs.readdir(path.join(process.cwd(), 'test'), (err, files) => {
-  const testFiles = files.
-    filter((file) => file.indexOf('-test') > -1).
-    map((file) => path.join('test', file));
+let assertions = [];
 
+function describe(name, fn) {
+  assertions.push({ name, fn });
+}
 
-  const testFilePromises = testFiles.map((file) => System.import(file));
+export { describe, assertions };
 
-  Promise.all(testFilePromises).then(() => {
-    const results = assertions.map(({ name, fn }) => {
-      let passed = true;
-      let error;
-      try {
-        fn.call();
-      } catch (e) {
-        passed = false;
-        error = e;
-      }
+const fileIsTest = (f) => f.indexOf('-test') > -1;
+const getFilePath = (dir, f) => path.join(dir, f);
 
-      return { name, passed, error };
+export function runTests({ directory }) {
+  fs.readdir(path.join(process.cwd(), directory), (err, files) => {
+    const testFiles = files.filter(fileIsTest).map(getFilePath.bind(null, directory));
+    const testFilePromises = testFiles.map(System.import);
+
+    Promise.all(testFilePromises).then(() => {
+      const results = assertions.map(({ name, fn }) => {
+        let passed = true;
+        let error;
+        try {
+          fn.call();
+        } catch (e) {
+          passed = false;
+          error = e;
+        }
+
+        return { name, passed, error };
+      });
+
+      results.forEach(({ name, passed, error}) => {
+        const colourFn = passed ? chalk.green : chalk.red;
+        const message = passed ? 'Pass: ' : 'Fail: ';
+
+        console.log(colourFn(message + name));
+        if (!passed) {
+          console.log('\t' + error.message);
+          console.log('\t' + error.stack);
+        }
+      });
+      console.log('');
+    }).catch((e) => {
+      console.log('Error loading test file');
+      console.log(e.message);
+      console.log(e.stack);
     });
-
-    results.forEach(({ name, passed, error}) => {
-      const colourFn = passed ? chalk.green : chalk.red;
-      const message = passed ? 'Pass: ' : 'Fail: ';
-
-      console.log(colourFn(message + name));
-      if (!passed) {
-        console.log('\t' + error.message);
-        console.log('\t' + error.stack);
-      }
-    });
-    console.log('');
-  }).catch((e) => {
-    console.log('Error loading test file');
-    console.log(e.message);
-    console.log(e.stack);
   });
-});
+}
